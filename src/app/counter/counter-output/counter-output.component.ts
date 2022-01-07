45bc588c4e0ba3a943ec6609f03e148f3173f194
reftene <reftene@deloittece.com>
2022-01-07 12:39:21 +0200

    counter example with ngrx store

---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CounterState } from '../state/counter.state';
+import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css'],
+})
+export class CounterOutputComponent implements OnInit {
+  counter$: Observable<{ counter: number }>;
+  constructor(private store: Store<{ counter: CounterState }>) {}
+
+  ngOnInit(): void {
+    this.counter$ = this.store.select('counter');
+  }
+}
